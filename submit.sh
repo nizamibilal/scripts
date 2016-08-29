9a9cff0cba5d2166385040e790b8c368ca8db2d7
Bilal Nizami <nizamibilal1064@gmail.com>
2016-08-29 21:06:37 +0200

    update

--- submit.sh
+++ submit.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+#+++++++++++++++++++++++++++++++++++++++++
+#
+#	This script submit 10 pbs pro job
+#   with different number of cpus (with the increment of 2)
+#	This could be used for benchmarking 
+#
+#+++++++++++++++++++++++++++++++++++++++++
 
-j=26
+j=2
 for i in {1..12}; do
     echo $j
     mkdir cpu_$j
     cd cpu_$j 
-    qsub -l select=2:ncpus=24:mpiprocs=24 ../amber_bm.pbs
+    qsub -l select=1:ncpus=24:mpiprocs=24 ../amber_bm.pbs
     ((j+=2)) 
     echo $j  
     cd ..
